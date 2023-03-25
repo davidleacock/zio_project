210fdb009ccbd7512200283d78bb7eb313c02fdb
davidleacock <david.leacock@innovapost.com>
2023-03-25 16:19:33 -0700

    Add another error to model

--- code/src/main/scala/domain/Models.scala
+++ code/src/main/scala/domain/Models.scala
@@ -6,4 +6,5 @@ sealed trait AppError extends Throwable
 
 object AppError {
   final case class RepositoryError(cause: Throwable) extends AppError
+  final case class DecodingError(message: String) extends AppError
 }
